928ecaeebb6c5fa385ca5a4b226e0114fc3885fd
Din <dinos4620@gmail.com>
2021-05-28 10:22:47 +0200

    friday....

--- src/cmps/StayFilter.jsx
+++ src/cmps/StayFilter.jsx
@@ -10,15 +10,18 @@ export class StayFilter extends Component {
         return (
             <section className="stay-filter flex">
                 <form className="flex">
-                    <div className="search-input flex column align-center justify-center">
-                        <label htmlFor="location">location</label>
+                    <div className="search-input flex column ">
+                        <div className="location-label">
+                            <label htmlFor="location">location</label>
+                        </div>
                         <div className="location">
                             <input autoComplete="off" type="text" name="search" id="location" placeholder="where do you want to go?" />
                         </div>
                     </div>
-                    
-                    <div className="search-input flex column align-center justify-center">
-                        <label>dates</label>
+                    <div className="search-input flex column ">
+                        <div className="dates-label">
+                            <label>dates</label>
+                        </div>
                         <div className="date-container flex">
                             <div className="check-in flex">
                                 <input autoComplete="off" type="text" name="search" id="dates" placeholder="check in" />
@@ -28,12 +31,15 @@ export class StayFilter extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className="search-input flex column align-center justify-center">
-                        <label htmlFor="guest">guests</label>
-                        <div className="guest">
+                    <div className="search-input flex column">
+                        <div className="guests-label">
+                            <label htmlFor="guest">guests</label>
+                        </div>
+                        <div className="guest-input">
                             <input autoComplete="off" type="text" name="search" id="guest" placeholder="1 guest" />
                         </div>
                     </div>
+                    <button className="primary-btn btn-grd">1</button>
                 </form>
             </section>
         )
