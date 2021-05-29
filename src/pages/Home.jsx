8b20df3561eae6c36cded682d316c1a9bc6df4be
Din <dinos4620@gmail.com>
2021-05-30 00:55:18 +0200

    includ home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,41 @@
 import { CityCard } from '../cmps/CityCard'
+import { connect } from 'react-redux'
+import { getStays } from '../store/actions/stay.actions.js'
+import { StayList } from '../cmps/StayList'
 
-export function Home() {
+function _Home({ stays }) {
+    const filterStays = stays.slice(0, 4)
+
     return (
-        <section className="stay-home">
-            <div className="home-hero">1</div>
+        <section className="stay-home main-container full">
+            <div className="home-hero full"></div>
             <h2>Explore new places</h2>
-            <CityCard title={'amsterdam'} imgUrl={'https://res.cloudinary.com/duhcvi6p4/image/upload/v1622299830/amsterdam-3410113_1920_aqdzxr.jpg'}/>
+            <div className="city-card-container">
+                <CityCard title={'amsterdam'} imgUrl={'amsterdam_nqxozm'} />
+                <CityCard title={'paris'} imgUrl={'paris_lljj5s'} />
+                <CityCard title={'new-york'} imgUrl={'new-york_en6f3a'} />
+                <CityCard title={'tokyo'} imgUrl={'tokyo_cl5jez'} />
+                <CityCard title={'london'} imgUrl={'london_ddezqi'} />
+                <CityCard title={'barcelona'} imgUrl={'barcelona_aq6xpb'} />
+            </div>
+            <div className="best-loc justify-center flex">
+                <StayList stays={filterStays}/>
+            </div>
+            <div className="become-host">
+                <h2>become a host</h2>
+            </div>
         </section>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        stays: state.stayModule.stays
+    }
+}
+
+const mapDispatchToProps = {
+    getStays
+}
+
+export const Home = connect(mapStateToProps, mapDispatchToProps)(_Home)
