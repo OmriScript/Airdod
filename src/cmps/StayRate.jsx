cbe11e134eb2e720a45301e70c1fef52e87cdc29
danielsheetrit <danielshitrit022@gmail.com>
2021-05-29 23:32:24 +0300

    daniel 23:32

--- src/cmps/StayRate.jsx
+++ src/cmps/StayRate.jsx
@@ -1,17 +1,19 @@
-import StarRate from '@material-ui/icons/StarRate'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export function StayRate({ stay, isShowReviews }) {
     const reviews = stay.reviews ? stay.reviews.length : 0
     let avgRate = stay.reviews ? stay.reviews.reduce((sum, obj) => {
         return sum + obj.rate;
     }, 0) / stay.reviews.length : 0
+    
     if (avgRate % 1 !== 0) { // if decimal
         avgRate = avgRate.toFixed(2)
     }
 
     return (
         <section className='stay-rate flex'>
-            <StarRate style={{ color: 'rgba(111, 52, 250, 1)' }} />
+            <FontAwesomeIcon icon={faStar} />
             {avgRate} ({reviews}{isShowReviews ? ' reviews' : ''})
         </section>
     )
